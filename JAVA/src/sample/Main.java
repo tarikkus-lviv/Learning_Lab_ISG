64679da8da92d13220c6711a6a86772efe77f1ba
Taras Kutsiaba <tarikkus24@gmail.com>
2020-10-12 19:40:40 +0300

    first java homework

---
+++ JAVA/src/sample/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main (String[] args)
+    {
+        for (int i=2; i<=100; i++)
+        {
+            for (int j=2; j<=i; j++)
+            {
+                if(j==i)
+                {
+                    System.out.println(i);
+                }
+                else if(i%j == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
